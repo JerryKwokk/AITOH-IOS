6f39f31e0de0ba73fdf7c5ab05bf3e06a4d07109
Jerry Kwok <jerrykwok.khh@gmail.com>
2020-02-21 12:43:41 +0800

    initial map album

---
+++ AITOH/Photo Album/MapMakerView.swift
@@ -0,0 +1,44 @@
+//
+//  MapMakerView.swift
+//  AITOH
+//
+//  Created by Jerry Kwok on 20/2/2020.
+//  Copyright © 2020 AITOH. All rights reserved.
+//
+
+import UIKit
+
+class MapMakerView: UIView {
+
+    var img: UIImage!
+    var borderColor: UIColor!
+    
+    init(frame: CGRect, image: UIImage, borderColor: UIColor) {
+        super.init(frame: frame)
+        self.img=image
+        self.borderColor=borderColor
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        let imgView = UIImageView(image: img)
+        imgView.frame=CGRect(x: 0, y: 0, width: 50, height: 50)
+        imgView.layer.cornerRadius = 25
+        imgView.layer.borderColor=borderColor?.cgColor
+        imgView.layer.borderWidth=4
+        imgView.clipsToBounds=true
+        let lbl=UILabel(frame: CGRect(x: 0, y: 45, width: 50, height: 10))
+        lbl.text = "▾"
+        lbl.font=UIFont.systemFont(ofSize: 24)
+        lbl.textColor = borderColor
+        lbl.textAlignment = .center
+        
+        self.addSubview(imgView)
+        self.addSubview(lbl)
+    }
+
+}
---
+++ AITOH/Photo Album/PhotoDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoDetailViewController.swift
+//  AITOH
+//
+//  Created by Jerry Kwok on 20/2/2020.
+//  Copyright © 2020 AITOH. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
